a99e2fc9d558558d62e9df385331c54aaaa59192
Er Pushpendra kumar gaur <pkg201297@gmail.com>
2024-03-12 10:50:55 +0530

    add new name

--- src/main/java/pageClass/pageText.java
+++ src/main/java/pageClass/pageText.java
@@ -28,6 +28,12 @@ public class pageText extends louchBroswer {
 		
 		crossIcon.click();
 		practice.click();
+		
+		System.out.println("my name is pushpendra kumar gaur");
+		
+		System.out.println("my name is ankit");
+		
+		
 	}
 
 }
